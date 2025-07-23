1d8327e9f8698b8a1e5ab6429005b5c2056ab51f
BangJeongBin <bangjungbin@gmail.com>
2025-07-23 21:30:07 +0900

    ApplicationContextBasicFindTest 테스트코드 빈 이름을 조회 X 성공

--- src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -6,9 +6,11 @@ import hello.core.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ApplicationContextBasicFindTest {
 
@@ -33,5 +35,13 @@ public class ApplicationContextBasicFindTest {
     void findBeanByName2() {
         MemberService memberService = ac.getBean("memberService", MemberServiceImpl.class);
         assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름을 조회 X")
+    void findBeanByNameX() {
+        // ac.getBean("XXXX", MemberService.class);
+        assertThrows(NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("XXXX", MemberServiceImpl.class));
     }
 }
