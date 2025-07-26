db37202b3d78d12859257a7f0b78ffa62147df59
BangJeongBin <bangjungbin@gmail.com>
2025-07-26 21:27:06 +0900

    XmlAppContext 생성

---
+++ src/test/java/hello/core/xml/XmlAppContext.java
@@ -0,0 +1,14 @@
+package hello.core.xml;
+
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class XmlAppContext {
+
+
+}
