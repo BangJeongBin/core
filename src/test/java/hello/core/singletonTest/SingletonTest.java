b0278dc0e6bcfe52bec41d3b31a5e773c3fb0baf
BangJeongBin <bangjungbin@gmail.com>
2025-07-29 21:40:57 +0900

    springContainer 테스트 성공 2

--- src/test/java/hello/core/singletonTest/SingletonTest.java
+++ src/test/java/hello/core/singletonTest/SingletonTest.java
@@ -54,7 +54,8 @@ public class SingletonTest {
         MemberService memberService1 = ac.getBean("memberService", MemberService.class);
         MemberService memberService2 = ac.getBean("memberService", MemberService.class);
 
-        // 참조값이 다른 것을 확인
+        // 참조값이 같은 것을 확인
+        // 스프링이 자동으로 싱글톤으로 객체를 생성
         System.out.println("memberService1 = " + memberService1);
         System.out.println("memberService2 = " + memberService2);
 
