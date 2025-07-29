e9f984bddbb3573aff3f32ed1987c1787cf2df2c
BangJeongBin <bangjungbin@gmail.com>
2025-07-29 21:20:47 +0900

    singletonServiceTest 테스트 성공 2

--- src/test/java/hello/core/singletonTest/SingletonTest.java
+++ src/test/java/hello/core/singletonTest/SingletonTest.java
@@ -6,6 +6,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class SingletonTest {
 
     @Test
@@ -23,7 +25,7 @@ public class SingletonTest {
         System.out.println("memberService2 = " + memberService2);
 
         // memberService1 != memberService2
-        Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+        assertThat(memberService1).isNotSameAs(memberService2);
     }
 
     @Test
@@ -34,6 +36,11 @@ public class SingletonTest {
 
         System.out.println("singletonService1 = " + singletonService1);
         System.out.println("singletonService2 = " + singletonService2);
+
+        // same : ==
+        // equals : .equals
+        assertThat(singletonService1).isSameAs(singletonService2);
+
     }
 
 }
