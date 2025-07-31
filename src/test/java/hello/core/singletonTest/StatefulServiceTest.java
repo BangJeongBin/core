f82b3f89c8435da79d77f8e21df0aa5d47fa531a
BangJeongBin <bangjungbin@gmail.com>
2025-07-31 17:18:39 +0900

    StatefulServiceSingleton 테스트 성공

--- src/test/java/hello/core/singletonTest/StatefulServiceTest.java
+++ src/test/java/hello/core/singletonTest/StatefulServiceTest.java
@@ -1,7 +1,38 @@
 package hello.core.singletonTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 class StatefulServiceTest {
+
+    @Test
+    void StatefulServiceSingleton() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean(StatefulService.class);
+        StatefulService statefulService2 = ac.getBean(StatefulService.class);
+
+        // ThreadA : A사용자가 10,000원 주문
+        statefulService1.order("userA", 10000);
+        // ThreadB : B사용자가 20,000원 주문
+        statefulService1.order("userB", 20000);
+
+        // ThreadA : 사용자A 주문 금액 조회
+        int price = statefulService1.getPrice();
+        System.out.println("price = " + price); // 싱글톤의 단점
+
+        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+    }
+
+    static class TestConfig {
+
+        @Bean
+        public StatefulService statefulService() {
+            return new StatefulService();
+        }
+    }
 
 }
