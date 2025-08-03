6b8fbc78cf7ad15690b653f0e49da8896c225907
BangJeongBin <bangjungbin@gmail.com>
2025-08-03 21:35:33 +0900

    파라미터 변수 명을 수정하여 빈 충돌 해결

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,20 +9,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     //private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); // 새로운 할인 정책 시 메서드 변경
     private final MemberRepository memberRepository; // 생성자로 스프링 빈 생성자 final을 시용하여 불변함, 생성자에서 누락시 컴파일 오류
     private final DiscountPolicy discountPolicy;
 
-
     // AppConfig로 MemoryMemberRepository, FixDiscountPolicy 할당
     //@Autowired // 생성자가 1개 인 경우 @Autowired 생략 가능
-//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) { // lombok 사용으로 주석처리
-//        this.memberRepository = memberRepository;
-//        this.discountPolicy = discountPolicy;
-//    }
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy rateDiscountPolicy) { // lombok 사용으로 주석처리
+        this.memberRepository = memberRepository;
+        this.discountPolicy = rateDiscountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
