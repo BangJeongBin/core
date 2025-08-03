53bffbb6ae5b0b96e1354ffd03a453ac092f7b1f
BangJeongBin <bangjungbin@gmail.com>
2025-08-03 21:22:53 +0900

    lombok을 사용하여 리팩토링

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -4,22 +4,25 @@ import com.sun.source.tree.UsesTree;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     //private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); // 새로운 할인 정책 시 메서드 변경
-    private final MemberRepository memberRepository;
+    private final MemberRepository memberRepository; // 생성자로 스프링 빈 생성자 final을 시용하여 불변함, 생성자에서 누락시 컴파일 오류
     private final DiscountPolicy discountPolicy;
+
 
     // AppConfig로 MemoryMemberRepository, FixDiscountPolicy 할당
     //@Autowired // 생성자가 1개 인 경우 @Autowired 생략 가능
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) { // lombok 사용으로 주석처리
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
